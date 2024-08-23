f564fd84ae9ef110841c520dbfb9b2ef0e80240c
lucas2003a <lucasatuncar1@gmail.com>
2024-08-22 22:35:30 -0500

    validation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,5 @@
 import { Component } from '@angular/core';
-import { FormControl, FormGroup } from '@angular/forms';
+import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +8,45 @@ import { FormControl, FormGroup } from '@angular/forms';
 })
 export class AppComponent {
   title = 'Angular-TEST-0.2';
+  students: any[] = [];
 
   LoginForm : FormGroup = new FormGroup({
-    email :  new FormControl(''),
-    password : new  FormControl('')
+    email :  new FormControl('', [Validators.required, Validators.email]),
+    password : new  FormControl('', Validators.required)
   });
 
+  DeleteStudent(index: number){
+    this.students.splice(index,1);
+  }
   OnSubmit(){
-    console.log(this.LoginForm.value);
+    console.log(this.LoginForm.valid);  // => ¿La validación formulario es válida? (true o false)
+    console.log(this.LoginForm.invalid);  // => ¿La validación de formulario es invalida? (true o false)
+    console.log(this.LoginForm.value);  // => Valores ingresados en el formulario
+
+    if(this.LoginForm.invalid){
+
+      if(this.LoginForm.get("email")?.errors){
+
+        let error = this.LoginForm.get("email")?.errors;
+
+        if(error?.["required"]){
+          alert("El campo email es requerido");
+        }else if(error?.["email"]){
+          alert("El campo email debe ser un correo electrónico");
+        }
+      }else if (this.LoginForm.get("password")?.errors){
+        let error = this.LoginForm.get("password")?.errors;
+
+        if(error?.["required"]){
+          alert("El campo password es requerido");
+        }
+      }else{
+        alert("Completa el formulario");
+      }
+      return;
+    }
+
+    alert("Formulario validado");
+    this.students.push(this.LoginForm.value);
   }
 }
